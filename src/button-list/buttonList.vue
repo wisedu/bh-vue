cf28176126647f69172e7ebf340538a1b45ff953
qintianxiang <txqin@wisedu.com>
2016-10-11 15:31:26 +0800

    update button list

--- src/button-list/buttonList.vue
+++ src/button-list/buttonList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="opt-buttons bh-mv-16">
+  <div class="opt-buttons">
     <a href="javascript:void(0);" class="bh-btn bh-btn-{{item.primary?'primary':'default'}} waves-effect" @click='click(item.clickEvent)' v-for="item in buttonList">
       <i class="iconfont icon-{{item.icon}}" v-if="item.icon"></i> {{item.text}}
     </a>
@@ -8,6 +8,7 @@
 <style scoped lang="sass">
 .opt-buttons {
   margin-left: -4px;
+  clear: both;
 }
 </style>
 <script>
